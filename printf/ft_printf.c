916d020c0e1a63fb832f40322ce88aab73456dca
Emilien Waltz <emilienwaltz@gmail.com>
2025-12-02 16:18:42 +0100

    started to add some logic

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,32 +6,65 @@
 /*   By: ewaltz <ewaltz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 13:09:30 by ewaltz            #+#    #+#             */
-/*   Updated: 2025/12/02 13:43:42 by ewaltz           ###   ########.fr       */
+/*   Updated: 2025/12/02 16:18:08 by ewaltz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+//#include "libftprintf.h"
+#include <unistd.h>
+#include <stdarg.h>
 
-int	ft_router()
+int	ft_router(const char spec, va_list *args)
+{
+  int	count;
+
+  count = 0;
+  if (spec == 'c')
+	count += ft_printchar(va_list *args);
+  if (spec == 's')
+	count += ft_printchar(va_list *args);
+  if (spec == 'p')
+	count += ft_printchar(va_list *args);
+  if (spec == 'd')
+	count += ft_printchar(va_list *args);
+  if (spec == 'i')
+	count += ft_printchar(va_list *args);
+  if (spec == 'u')
+	count += ft_printchar(va_list *args);
+  if (spec == 'x')
+	count += ft_printchar(va_list *args);
+  if (spec == 'X')
+	count += ft_printchar(va_list *args);
+  return (count);
+}
 
 int	ft_printf(const char *str, ...)
 {
   int	i;
+  int	count;
+  va_list args;
 
+  va_start(args, str);
   i = 0;
+  count = 0;
   while (str[i])
   {
-	if (str[i] == '%')
+	if (str[i] == '%' && str[i + 1] != '%')
 	{
-	  if (str[i + 1] == 'c')
-		ft_printf_char(????);
-	  if (str[i + 1] == 's')
-		ft_printf_char(????);
-	  if (str[i + 1] == 'p')
-		ft_printf_char(????);
-	  if (str[i + 1] == 'd')
-		ft_printf_char(????);
-
+	  count += ft_router(str[i + 1], &args);
+	  i += 2;
+	  continue;
 	}
+	write (1, &str[i], 1);
+	i++;
+	count++;
   }
+  va_end(args);
+  return (count)
+}
+
+
+int main()
+{
+  ft_printf("salut");
 }
