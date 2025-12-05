eaa058982819c870c18b5ad1ce544971cd34aadb
Emilien Waltz <emilienwaltz@gmail.com>
2025-12-05 12:32:08 +0100

    finitoooo

--- printf/ft_printf_hexa_low.c
+++ printf/ft_printf_hexa_low.c
@@ -1,46 +1,46 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf_hexa_low.c                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ewaltz <ewaltz@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/02 16:52:28 by ewaltz            #+#    #+#             */
-/*   Updated: 2025/12/04 11:26:48 by ewaltz           ###   ########.fr       */
-/*                                                                            */
+/*																			  */
+/*														  :::	   ::::::::   */
+/*	 ft_printf_hexa_low.c								:+:		 :+:	:+:   */
+/*													  +:+ +:+		  +:+	  */
+/*	 By: ewaltz <ewaltz@student.42.fr>				+#+  +:+	   +#+		  */
+/*												  +#+#+#+#+#+	+#+			  */
+/*	 Created: 2025/12/02 16:52:28 by ewaltz			   #+#	  #+#			  */
+/*	 Updated: 2025/12/04 11:26:48 by ewaltz			  ###	########.fr		  */
+/*																			  */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 int	ft_putnbr_base_low(unsigned long nbr)
 {
-  char	c;
-  int	count;
-  char *base = "0123456789abcdef";
+	char	c;
+	int		count;
+	char	*base;
 
-  count = 0;
-  c = 'x';
-  if (nbr >= 16)
-  {
-	count += ft_putnbr_base_low(nbr / 16);
-	count += ft_putnbr_base_low(nbr % 16);
-  }
-  else
-  {
-	c = base[nbr];
-	count += write (1, &c, 1);
-  }
-  return (count);
+	base = "0123456789abcdef";
+	count = 0;
+	c = 'x';
+	if (nbr >= 16)
+	{
+		count += ft_putnbr_base_low(nbr / 16);
+		count += ft_putnbr_base_low(nbr % 16);
+	}
+	else
+	{
+		c = base[nbr];
+		count += write (1, &c, 1);
+	}
+	return (count);
 }
 
 int	ft_printf_hexa_low(va_list *args)
 {
-  int	count;
-  unsigned int i;
+	int				count;
+	unsigned int	i;
 
-  count = 0;
-  i = va_arg(*args, unsigned int);
-  count += ft_putnbr_base_low((long)i);
-  return (count);
-
+	count = 0;
+	i = va_arg(*args, unsigned int);
+	count += ft_putnbr_base_low((long)i);
+	return (count);
 }
